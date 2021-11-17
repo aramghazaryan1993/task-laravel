548b283c63dccd06ed9f263da58b7c22d0c9d89f
aramghazaryan1993 <91990675+aramghazaryan1993@users.noreply.github.com>
2021-11-18 01:06:22 +0400

    Update UserData.php

--- app/Http/Resources/UserData.php
+++ app/Http/Resources/UserData.php
@@ -15,12 +15,5 @@ class UserData extends JsonResource
     public function toArray($request)
     {
       return parent::toArray($request);
-        return [
-            'id' => $this->id,
-            'name' => $this->name,
-            'description' => $this->detail,
-            'created_at' => $this->created_at->format('d/m/Y'),
-            'updated_at' => $this->updated_at->format('d/m/Y'),
-        ];
     }
 }
