acea9b8dbd3e1e3b9c35342f962507f0dbd58388
Aram <aramghazaryan1993>
2021-11-28 23:57:38 +0400

    change2

--- database/migrations/2021_11_17_111010_create_user_tag_rels_table.php
+++ database/migrations/2021_11_17_111010_create_user_tag_rels_table.php
@@ -16,9 +16,9 @@ class CreateUserTagRelsTable extends Migration
         Schema::create('user_tag_rels', function (Blueprint $table) {
             $table->id();
             $table->bigInteger('tag_id')->unsigned();
-            $table->bigInteger('product_id ')->unsigned();
+            $table->bigInteger('product_id')->unsigned();
             $table->timestamps();
-            $table->foreign('product_id ')->references('id')->on('product')->onDelete('cascade');
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
             $table->foreign('tag_id')->references('id')->on('tags')->onDelete('cascade');
         });
     }
