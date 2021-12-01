12dfaba83e3be6fcbfe6046887e99ffae487e98a
Aram <aramghazaryan1993>
2021-12-01 16:09:35 +0400

    fix: I changed the names of the variables

--- resources/views/email/send-email.blade.php
+++ resources/views/email/send-email.blade.php
@@ -1,6 +1,6 @@
 
 <h1>Product</h1>
 
-<h2>Name: <span>{{$mailData['name']}}</span></h2>
-<h2>description: <span>{{$mailData['description']}}</span></h2>
+<h2>Name: <span>{{$name}}</span></h2>
+<h2>description: <span>{{$description}}</span></h2>
 
