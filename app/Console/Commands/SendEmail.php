50580bd08e4e4599a932d39d84c7c3e162a91dd6
Aram <aramghazaryan1993>
2021-12-03 11:30:22 +0400

    fix: handle() sends email asynchronously

--- app/Console/Commands/SendEmail.php
+++ app/Console/Commands/SendEmail.php
@@ -6,6 +6,7 @@ use App\Models\Product;
 use Illuminate\Console\Command;
 use App\Mail\SendEmail as SendEmailMailable;
 use Illuminate\Support\Facades\Mail;
+//use App\Jobs\SendEmailJob;
 
 /**
  * Class SendEmail
@@ -27,17 +28,30 @@ class SendEmail extends Command
      */
     protected $description = 'Send email my product';
 
-
     public function handle()
     {
         $product = Product::all()->first();
 
-       if(!is_null($product)){
-           Mail::to($this->argument('email'))->send(new SendEmailMailable($product->name, $product->description));
-           return $this->info('Success');
-       }else{
-           return $this->info('Product does not exist');
-       }
+//       if(!is_null($product)){
+//           Mail::to($this->argument('email'))->send(new SendEmailMailable($product->name, $product->description));
+//           return $this->info('Success');
+//       }else{
+//           return $this->info('Product does not exist');
+//       }
 
+//       if(!is_null($product)){
+//            dispatch(new SendEmailJob($product->name, $product->description,$this->argument('email')));
+//           return $this->info('Success');
+//       }else{
+//           return $this->info('Product does not exist');
+//       }
+
+
+        if(!is_null($product)){
+            Mail::to($this->argument('email'))->queue((new SendEmailMailable($product->name, $product->description))->onQueue('emails')->delay(5));
+            return $this->info('Success');
+        }else{
+            return $this->info('Product does not exist');
+        }
     }
 }
