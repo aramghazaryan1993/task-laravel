5172a29850686441977af8fe276caf03aa05a7b2
Aram <aramghazaryan1993>
2021-12-03 11:34:34 +0400

    fix: add queue value 'beanstalkd' => ['queue' => ['email'] ]

--- config/queue.php
+++ config/queue.php
@@ -45,7 +45,7 @@ return [
         'beanstalkd' => [
             'driver' => 'beanstalkd',
             'host' => 'localhost',
-            'queue' => 'default',
+            'queue' => ['emails'],
             'retry_after' => 90,
             'block_for' => 0,
             'after_commit' => false,
