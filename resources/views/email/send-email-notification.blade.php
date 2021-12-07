197abf175a309248d526e4fbf71cbd032f3a3cc1
Aram <aramghazaryan1993>
2021-12-07 18:47:32 +0400

    feat: Created mail tamplate notification

---
+++ resources/views/email/send-email-notification.blade.php
@@ -0,0 +1,7 @@
+
+<h1>Product</h1>
+
+<h2>Name: <span>{{$name}}</span></h2>
+<h2>description: <span>{{$description}}</span></h2>
+
+<h2>Image: </h2> <img src="http://task-laravel.loc/{{$image}}" alt="">
