98e7e75772db8683a428c840a99e5f73383e20ae
Developer <Aram>
2021-12-08 16:45:19 +0400

    fix remove link

--- resources/views/email/send-email-notification.blade.php
+++ resources/views/email/send-email-notification.blade.php
@@ -2,6 +2,7 @@
 <h1>Product</h1>
 
 <h2>Name: <span>{{$name}}</span></h2>
+
 <h2>description: <span>{{$description}}</span></h2>
-<a href="{{asset($image)}}">{{$image}}</a>
+
 <h2>Image url:  <span>{{$image}}</span> </h2>
