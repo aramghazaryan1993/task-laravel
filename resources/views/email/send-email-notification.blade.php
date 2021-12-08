2838085f22112d7432ea39aa33335d85c8fa6200
Aram <aramghazaryan1993>
2021-12-08 16:32:50 +0400

    test: Test code

--- resources/views/email/send-email-notification.blade.php
+++ resources/views/email/send-email-notification.blade.php
@@ -3,5 +3,5 @@
 
 <h2>Name: <span>{{$name}}</span></h2>
 <h2>description: <span>{{$description}}</span></h2>
-
+<a href="{{asset($image)}}">{{$image}}</a>
 <h2>Image url:  <span>{{$image}}</span> </h2>
