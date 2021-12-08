91fd227294c525d20922b37b055ad5a671f53a30
Aram <aramghazaryan1993>
2021-12-08 16:38:16 +0400

    fix: changed  'url' => env('APP_URL', 'http://task-laravel.loc/')

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'http://task-laravel.loc/'),
 
     'asset_url' => env('ASSET_URL', null),
 
