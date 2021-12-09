6f552ecaec4024fe1d191eea698bdf800e8b0e5c
Developer <Aram>
2021-12-09 12:54:05 +0400

    feat:

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -18,6 +18,12 @@ class EventServiceProvider extends ServiceProvider
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
+        \App\Events\CreatedEventByCoproduct::class => [
+            \App\Listeners\CreatedEventListenerByCoproduct::class
+        ],
+        \App\Events\UpdatedEventByCoproduct::class => [
+            \App\Listeners\UpdatedEventListenerByCoproduct::class
+        ],
     ];
 
     /**
