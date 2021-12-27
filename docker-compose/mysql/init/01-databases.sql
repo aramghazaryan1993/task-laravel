a04aeb2f04bb530263e47bac5b585bf4fb199e41
aramghazaryan1993 <aramghazaryansoft@gmail.com>
2021-12-27 16:08:39 +0400

    delete  puth

--- docker-compose/mysql/init/01-databases.sql
+++
@@ -1,6 +0,0 @@
--- # create databases
--- CREATE DATABASE IF NOT EXISTS `test`;
---
--- #create task_laravel-docker user and grant rights
--- CREATE USER 'root'@'db' IDENTIFIED BY 'root';
--- GRANT ALL PRIVILEGES ON *.* TO 'test'@'%';
