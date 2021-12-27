2b064ff84d2ea9e037ae63a7093d454e0139c8f1
aramghazaryan1993 <aramghazaryansoft@gmail.com>
2021-12-27 16:02:09 +0400

    delete file

---
+++ docker/mysql/init/01-databases.sql
@@ -0,0 +1,6 @@
+-- # create databases
+-- CREATE DATABASE IF NOT EXISTS `test`;
+--
+-- #create task_laravel-docker user and grant rights
+-- CREATE USER 'root'@'db' IDENTIFIED BY 'root';
+-- GRANT ALL PRIVILEGES ON *.* TO 'test'@'%';
